9b0338bb60625f25fa66c8e00ad24c425fdca8f4
Anna Lezhikova <anna.v.l@gmail.com>
2015-09-30 12:13:51 +1300

    Moce bundle.js to assets folder

--- public/index.js
+++ public/index.js
@@ -1,6 +1,11 @@
 var React = require('react')
 var MeetupCreate = require('./source/components/meetupCreate')
-React.render(
+var domready = require('domready')
+
+domready(function () {
+ React.render(
   <MeetupCreate />,
   document.getElementById('meetup-form')
-);
+  )
+})
+
